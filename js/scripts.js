1e23c60d5bcff398b38704dcf910bccdacd3756c
Ram Zallan <ramzallan@gmail.com>
2017-10-05 16:49:57 -0400

    Finishing up presentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,27 @@
-const transitionTime = 200;
+const transitionTime = 1;
+let currScroll = 0;
 $(document).keydown(function(e) {
-     let key = e.which;
-      if(key == 40) {
-          e.preventDefault();
-          var pageHeight = $(window).height();
-          $('html, body').animate({
-              scrollTop: pageHeight
-          }, transitionTime);
-          return false;
-      }
-       if(key == 38) {
-           e.preventDefault();
-           var pageHeight = $(window).height();
-           $('html, body').animate({
-               scrollTop: -pageHeight
-           }, transitionTime);
-           return false;
-       }
+    let key = e.which;
+    if (key == 40) {
+        e.preventDefault();
+        var pageHeight = $(window).height();
+        $('html, body').animate({
+            scrollTop: currScroll + pageHeight
+        }, transitionTime);
+        if (currScroll >= $(document).height() - pageHeight) {
+            return true;
+        }
+        currScroll += pageHeight;
+    }
+    if (key == 38) {
+        e.preventDefault();
+        var pageHeight = $(window).height();
+        $('html, body').animate({
+            scrollTop: currScroll - pageHeight
+        }, transitionTime);
+        if (currScroll == 0) {
+            return true;
+        }
+        currScroll -= pageHeight;
+    }
 });
